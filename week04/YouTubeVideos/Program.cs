dbc8a2e085e4cf9c1d7201b22a18ee2a62848ee9
jaykee28 <jmufetu@gmail.com>
2025-05-29 20:42:59 +0200

    Finished YouTube Videos

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,31 @@
-using System;
-
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("Learn C# Basics", "Programming Academy", 600);
+        video1.AddComment(new Comment("Albert", "Great tutorial!"));
+        video1.AddComment(new Comment("Mike", "Very helpful."));
+        video1.AddComment(new Comment("Patie", "Clear explanations!"));
+
+        Video video2 = new Video("Python Programming For Beginners", "Jacqueline Mufetu", 750);
+        video2.AddComment(new Comment("Jackie", "Python finally makes sense!"));
+        video2.AddComment(new Comment("Emma", "Concise and informative."));
+        video2.AddComment(new Comment("Lori", "Loved this video!"));
+
+        Video video3 = new Video("Debugging Tips for Developers", "Albert Bungane", 500);
+        video3.AddComment(new Comment("Grace", "Super useful tips!"));
+        video3.AddComment(new Comment("Emmy", "I needed this."));
+        video3.AddComment(new Comment("Renny", "Very insightful."));
+
+        videos.Add(video1);
+        videos.Add(video2);
+        videos.Add(video3);
+
+        foreach (var video in videos)
+        {
+            video.DisplayInfo();
+        }
     }
 }
