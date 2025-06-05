826ef1479eed690b3c016fda32c84d2567743a4c
jaykee28 <jmufetu@gmail.com>
2025-06-05 17:43:26 +0200

    Finished ExerciseTracking

---
+++ week07/ExerciseTracking/Cycling.cs
@@ -0,0 +1,13 @@
+class Cycling : Activity
+{
+    private double speed;
+
+    public Cycling(DateTime date, int minutes, double speed) : base(date, minutes)
+    {
+        this.speed = speed;
+    }
+
+    public override double GetDistance() => (speed * Minutes) / 60;
+    public override double GetSpeed() => speed;
+    public override double GetPace() => 60 / speed;
+}
